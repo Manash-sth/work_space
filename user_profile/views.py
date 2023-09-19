94595a13b0223fa259bb29e13bc18582ee7a2688
Manash-sth <manash.81142615@gmail.com>
2023-09-19 11:52:22 +0545

    first commit..

---
+++ user_profile/views.py
@@ -0,0 +1,6 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class TestApi(APIView):
+    def get(self, rrequest):
+        return Response({"profile": "HelloWorld"})
